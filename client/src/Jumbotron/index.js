ea471f3bd6b2f70bfea7e12b79bfea31468d0fc3
JohnMohr <74803311+JohnMohr@users.noreply.github.com>
2021-02-23 20:11:37 -0800

    created jumbotron component

---
+++ client/src/Jumbotron/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Jumbotron() {
+    return (
+        <div>
+            <div className="jumbotron">
+                <h1 className="display-4 text-center titleText">Google Book Shelf</h1>
+                <br/>
+                <h2 className="text-center titleText">Search for books and add them to the Shelf.</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Jumbotron;
