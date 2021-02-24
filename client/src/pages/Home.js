e92f408bb0df5539f14d56713a7683f110be9757
JohnMohr <74803311+JohnMohr@users.noreply.github.com>
2021-02-24 09:28:53 -0800

    created page folder with home wip. created router.

---
+++ client/src/pages/Home.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import { Container } from "../components/Grid";
+import Nav from "../components/Nav";
+import Jumobotron from "../components/Jumbotron";
+import { Import, SubmitBtn } from "../components/Search";
+import API from "../utils/API";
