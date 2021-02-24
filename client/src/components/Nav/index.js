be03a343f135f94d577a83aa464b90595d201c7e
JohnMohr <74803311+JohnMohr@users.noreply.github.com>
2021-02-23 20:03:48 -0800

    linked bootstrap to public html and created Nav component

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Nav() {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark">
+                <Link className="navbar-brand" to="/">Book Shelf</Link>
+                <button className="navbar-toggler">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
+                            <Link className="nav-link" to="/">Home<span className="sr-only">(current)</span></Link>
+                        </li>
+                        <li className="nav-item active">
+                            <Link className="nav-link" to="/saved">Shelf</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Nav;
