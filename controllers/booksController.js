b8aff86b99b09dace144db786dfd54ff8e200e1c
JohnMohr <74803311+JohnMohr@users.noreply.github.com>
2021-02-27 10:22:30 -0800

    debug deployment

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,30 +1,20 @@
-const db = require("../models");
+import axios from "axios";
 
-module.exports = {
-    findAll: function(req, res) {
-      db.GoogleBooks
-        .find(req.query)
-        .sort({ date: -1 })
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-    },
-    findOne: function(req, res) {
-      db.GoogleBooks
-        .findOne({id: req.params.id})
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-    },
-    save: function(req, res) {
-      db.GoogleBooks
-        .create(req.body)
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-    },
-    remove: function(req, res) {
-      db.GoogleBooks
-        .findById({ _id: req.params.id })
-        .then(dbModel => dbModel.remove())
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-    }
-  };
+export default {
+  // Gets all books
+  getBooks: function() {
+    return axios.get("/api/books");
+  },
+  // Gets the book with the given id
+  getBook: function(id) {
+    return axios.get("/api/books/" + id);
+  },
+  // Deletes the book with the given id
+  deleteBook: function(id) {
+    return axios.delete("/api/books/" + id);
+  },
+  // Saves a book to the database
+  saveBook: function(bookData) {
+    return axios.post("/api/books", bookData);
+  }
+};
--- routes/api/books.js
+++ routes/api/books.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const booksController = require("../../controllers/booksController");
+const { default: axios } = require("axios");
 
 // Matches with "/api/books"
 router.route("/")
@@ -7,9 +8,20 @@ router.route("/")
   .post(booksController.create);
 
 // Matches with "/api/books/:id"
-router.route("/:id")
+router
+  .route("/:id")
   .get(booksController.findById)
   .put(booksController.update)
   .delete(booksController.remove);
 
 module.exports = router;
+
+router.post("/", (req,res) => {
+axios.get('https://www.googleapis.com/books/v1/volumes?q=' + req.body.name)
+.then((response => response.json(response.data)) 
+.then(result => {
+  this.setState({ books: result.items})
+  // console.log(response.data);
+}));
+
+});
